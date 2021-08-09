af51a4776600309d4bf5a725cfba63dcbb8bffb8
Simeon Markov <moni300403@abv.bg>
2021-08-09 19:15:40 +0300

    Started working on the foundations of the project
    Had some technical difficulties with Port 5001, going to restart the PC to see if the problem is resolved

--- LBL/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ LBL/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace LBL.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(LBLDbContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
--- LBL/Startup.cs
+++ LBL/Startup.cs
@@ -1,6 +1,7 @@
 namespace LBL
 {
     using LBL.Data;
+    using LBL.Infrastructure;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Identity;
@@ -18,7 +19,7 @@ namespace LBL
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options
+            services.AddDbContext<LBLDbContext>(options => options
                 .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             
             services.AddDatabaseDeveloperPageExceptionFilter();
@@ -31,7 +32,7 @@ namespace LBL
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequireUppercase = false;
                 })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<LBLDbContext>();
 
             services
                 .AddControllersWithViews();
@@ -39,6 +40,8 @@ namespace LBL
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.PrepareDatabase();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
